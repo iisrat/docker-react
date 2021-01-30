0e0dc0b2d539f5286ed25ca337322c35e2291203
isratzaman <israt.zaman@welldev.io>
2021-01-30 23:06:58 +0600

    please deploy

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello there!!!/i);
+  const linkElement = screen.getByText(/Hello there, Please deploy!!!/i);
   expect(linkElement).toBeInTheDocument();
 });
 
