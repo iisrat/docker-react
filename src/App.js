4a8e79c6bf8372f9cb40eaec0ad9f82e888edd5b
isratzaman <israt.zaman@welldev.io>
2021-01-30 22:39:09 +0600

    please deploy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello there!!!
+          Hello there, Please deploy!!!
         </a>
       </header>
     </div>
